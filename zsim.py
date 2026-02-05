1de97465b4f265ba06bd08501d86ac458fd277e2
卢天宇 <tianyulu@pony.ai>
2026-02-05 14:50:48 +0800

    feat: use python script with flag instead of shell

---
+++ zsim.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+from absl import app, flags
+import subprocess
+import os
+
+DATA_LIST = [
+    '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260109/k9011-144638-1767941609.atomic.zip',
+    '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260102/k9043-102255-1767324907.atomic.zip',
+    '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260107/k9043-112624-1767763796.atomic.zip',
+    '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260102/k9042-094931-1767324699.atomic.zip',
+    # https://jira.corp.pony.ai/browse/RTI-42778510
+    '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260129/k9216-100455-1769655966.atomic.zip',
+    # 长距离，latency报告中表现最差的record
+    '/guangzhou/truncated_data_v2/manual/guangzhou/20250926/k8106-074958-1758847477__1758847974.atomic.zip',
+]
+USE_DATA_INDEX = 0
+
+flags.DEFINE_enum(
+    'mode',
+    'olive',
+    ['olive', 'profile'],
+    'Running mode',
+)
+
+flags.DEFINE_bool(
+    'build',
+    False,
+    'Whether to build before running binary',
+)
+
+flags.DEFINE_bool(
+    'geo',
+    True,
+    'Whether to enable geometry-based neighbor calculation',
+)
+
+flags.DEFINE_string(
+    'dir',
+    f'{DATA_LIST[USE_DATA_INDEX]}',
+    'The remote directory for record',
+)
+
+flags.DEFINE_bool(
+    'upload',
+    False,
+    'Whether to upload flame graph onto web',
+)
+
+FLAGS = flags.FLAGS
+
+
+# flags.alias_flag('g', 'enable_geometry')
+
+def main(argv):
+    os.chdir('/home/tianyulu/work/ponyai/.sub-repos')
+
+    if FLAGS.build:
+        if FLAGS.mode == 'olive':
+            subprocess.run(['make8', 'build', 'olive_main'], check=True)
+        elif FLAGS.mode == 'profile':
+            subprocess.run(['make8', 'build', 'simulation_main'], check=True)
+
+    olive_binary_path = './make8-bin/common/tools/olive/olive_main'
+    profiler_binary_path = './make8-bin/common/utils/profiler/profile_helper/profile_helper_cli_package'
+
+    olive_args = [
+        '--simple-mode=detected_map',
+        f'--remote-dir={FLAGS.dir}',
+        '--static_map_any_version',
+        '--road_graph_any_version',
+        '--simulation_data_buffer_size=512',
+        f'--enable_geometry_based_neighbor_calculation={str(FLAGS.geo).lower()}',
+    ]
+
+    profiler_args = [
+        'profile',
+        '--mode=cpu',
+        '--binary=./make8-bin/infrastructure/simulation/simulation_main',
+        f'--cwd={os.getcwd()}',
+        '--args',
+        ' '.join(olive_args),
+    ]
+
+    if FLAGS.mode == 'olive':
+        subprocess.run([olive_binary_path] + olive_args, check=True)
+    elif FLAGS.mode == 'profile':
+        subprocess.run([profiler_binary_path] + profiler_args, check=True)
+        if FLAGS.upload:
+            subprocess.run([profiler_binary_path, 'upload'], check=True)
+
+
+if __name__ == '__main__':
+    app.run(main)
