d5092cd3d39752144cd7d245f9115a5f00a9850f
卢天宇 <tianyulu@pony.ai>
2026-02-05 18:06:14 +0800

    feat: extract functions for different feature

--- zsim.py
+++ zsim.py
@@ -3,7 +3,7 @@ from absl import app, flags
 import subprocess
 import os
 
-DATA_LIST = [
+GEOMETRY_BASED_NEIGHBOR_CALCULATION_RELATED_ISSUE_REMOTE_DIRS = [
     '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260109/k9011-144638-1767941609.atomic.zip',
     '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260102/k9043-102255-1767324907.atomic.zip',
     '/daxing/sensitive_data/truncated_data_v2/issue_bot/disengagement/20260107/k9043-112624-1767763796.atomic.zip',
@@ -13,7 +13,7 @@ DATA_LIST = [
     # 长距离，latency报告中表现最差的record
     '/guangzhou/truncated_data_v2/manual/guangzhou/20250926/k8106-074958-1758847477__1758847974.atomic.zip',
 ]
-USE_DATA_INDEX = 0
+USE_DIR_INDEX = 0
 
 flags.DEFINE_enum(
     'mode',
@@ -36,7 +36,7 @@ flags.DEFINE_bool(
 
 flags.DEFINE_string(
     'dir',
-    f'{DATA_LIST[USE_DATA_INDEX]}',
+    f'{GEOMETRY_BASED_NEIGHBOR_CALCULATION_RELATED_ISSUE_REMOTE_DIRS[USE_DIR_INDEX]}',
     'The remote directory for record',
 )
 
@@ -51,7 +51,27 @@ FLAGS = flags.FLAGS
 
 # flags.alias_flag('g', 'enable_geometry')
 
-def main(argv):
+
+def common(argv):
+    os.chdir('/home/tianyulu/work/ponyai/.sub-repos')
+
+    if FLAGS.build:
+        subprocess.run(['make8', 'build', 'olive_main'], check=True)
+
+    olive_binary_path = './make8-bin/common/tools/olive/olive_main'
+
+    olive_args = [
+        '--simple-mode=detected_map',
+        f'--remote-dir={FLAGS.dir}',
+        '--static_map_any_version',
+        '--road_graph_any_version',
+        '--simulation_data_buffer_size=512',
+    ]
+
+    subprocess.run([olive_binary_path] + olive_args, check=True)
+
+
+def geometry_neighbor(argv):
     os.chdir('/home/tianyulu/work/ponyai/.sub-repos')
 
     if FLAGS.build:
@@ -90,4 +110,5 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    app.run(main)
+    # app.run(geometry_neighbor)
+    app.run(common)
